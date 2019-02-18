d232aa64460ddad4251acea763d0dd3773a63692
sky <1019722231@qq.com>
2019-02-19 07:00:18 +0800

    提交cloud

---
+++ microservice-discovery-eureka/src/main/java/com/pan/cloud/gateway/EurakaApplication.java
@@ -0,0 +1,20 @@
+package com.pan.cloud.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description: 用Spring Boot创建一个服务类EurekaServerApplication，需要一个注解@EnableEurekaServer加在springboot工程的启动类上
+ * Time:2019/2/11
+ *
+ * @author pan
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurakaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurakaApplication.class);
+    }
+}
